26feb9f92e3208e9813985af9106f2260f3566b1
sldirisala <145953282+sldirisala@users.noreply.github.com>
2023-09-25 20:50:58 +0530

    Add files via upload

---
+++ src/components/GradientLineChart.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="card">
+    <div class="pb-0 card-header mb-0">
+      <h6>{{ title }}</h6>
+      <p class="text-sm">
+        <i class="fa fa-arrow-up text-success"></i>
+        <span class="font-weight-bold">{{detail1}}</span>
+        {{detail2}}
+      </p>
+    </div>
+    <div class="p-3 card-body">
+      <div class="chart">
+        <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  name: "gradient-line-chart",
+
+  props: {
+    title: {
+      type: String,
+      default: "Attendance overview",
+    },
+    detail1: {
+      type: String,
+      default: "4% more",
+    },
+    detail2: {
+      type: String,
+      default: "in 2021",
+    },
+  },
+
+  mounted() {
+    var ctx1 = document.getElementById("chart-line").getContext("2d");
+
+    var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke1.addColorStop(1, "rgba(94, 114, 228, 0.2)");
+    gradientStroke1.addColorStop(0.2, "rgba(94, 114, 228, 0.0)");
+    gradientStroke1.addColorStop(0, "rgba(94, 114, 228, 0)");
+    new Chart(ctx1, {
+      type: "line",
+      data: {
+        labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [
+          {
+            label: "Student present",
+            tension: 0.4,
+            borderWidth: 0,
+            pointRadius: 0,
+            borderColor: "#4BB543 ",
+            backgroundColor: gradientStroke1,
+            // eslint-disable-next-line no-dupe-keys
+            borderWidth: 3,
+            fill: true,
+            data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+            maxBarThickness: 6,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        interaction: {
+          intersect: false,
+          mode: "index",
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5],
+            },
+            ticks: {
+              display: true,
+              padding: 10,
+              color: "#272727",
+              font: {
+                size: 11,
+                family: "Open Sans",
+                style: "normal",
+                lineHeight: 2,
+              },
+            },
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: false,
+              drawOnChartArea: false,
+              drawTicks: false,
+              borderDash: [5, 5],
+            },
+            ticks: {
+              display: true,
+              color: "#272727",
+              padding: 20,
+              font: {
+                size: 11,
+                family: "Open Sans",
+                style: "normal",
+                lineHeight: 2,
+
+              },
+            },
+          },
+        },
+      },
+    });
+  },
+};
+</script>
+<style scoped>
+  .card{
+    border-radius: 10px;
+    box-shadow: 0px 4px 8px rgba(0,0,0,0.2),
+    0 6px 20px 0 rgba(0,0,0,0.19);
+    padding: 10px;
+    color :#272727;
+  }
+</style>
